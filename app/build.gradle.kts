3f864165bc6511ce1908c88d229f02aca4ddd512
Hecker_01 <101524930+Hecker-01@users.noreply.github.com>
2026-03-05 14:52:08 +0100

    Bump version to 2.1, update color resources, and refine string values

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.heckr.comicconverter"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
