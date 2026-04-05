3c57f01ad64372532121005516ba47bf09458501
Hecker_01 <101524930+Hecker-01@users.noreply.github.com>
2026-04-05 20:48:03 +0200

    revert year versionname

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = gitCommitCount
-        versionName = "26.2.0"
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
